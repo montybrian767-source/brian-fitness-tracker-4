4f4d1a62bbf9f74fd86d4485243ddc1b0cc38a6c
montybrian767-source <montybrian767@yahoo.com>
2026-07-02 01:35:02 -0400

    Add files via upload

---
+++ engines/exercise_intelligence.py
@@ -0,0 +1,3 @@
+class ExerciseIntelligence:
+    def get_profile(self,name):
+        return {"name":name,"difficulty":"Beginner","primary":"Unknown","tips":["Control the movement","Use full range of motion"]}
